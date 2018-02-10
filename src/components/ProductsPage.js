e79e41636a984652b12dd4e852bedc864c75c4f5
Cathy Lee <cathylee@cathys-mbp.lan>
2018-02-09 22:12:46 -0800

    Semi styled product-items and added decimal to prices

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -17,17 +17,10 @@ class ProductsPage extends Component {
           		<div className="one-item">
           			<img className="product-image" src={require("../images/" + item.filename)} />
           			<p className="name">{item.name}</p>
-          			<p className="price">${item.price}</p>
+          			<p className="price">${item.price * .01}</p>
           			<button>Add to cart</button>
           		</div>
       		)}
-          	<div className="one-item">
-          		<img src={require("../images/" + this.state.data[0].filename)} />
-          		<p>{this.state.data[0].title}</p>
-          		<p className="product-name">{this.state.data[0].name}</p>
-          		<p className="product-price">Static price</p>
-          		<button>Add to cart</button>
-          	</div>	
           </div>
       </div>
     	)
